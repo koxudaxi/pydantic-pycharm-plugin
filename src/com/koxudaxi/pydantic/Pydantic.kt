940ac67432ea4351c09ae79cdec4d831b85a0e3b
Koudai Aono <koxudaxi@gmail.com>
2019-08-29 21:02:41 +0900

    add types on auto-completion

--- src/com/koxudaxi/pydantic/Pydantic.kt
+++ src/com/koxudaxi/pydantic/Pydantic.kt
@@ -2,6 +2,7 @@ package com.koxudaxi.pydantic
 
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.QualifiedName
+import com.jetbrains.python.documentation.PythonDocumentationProvider
 import com.jetbrains.python.psi.*
 import com.jetbrains.python.psi.resolve.PyResolveUtil
 import com.jetbrains.python.psi.types.TypeEvalContext
@@ -13,32 +14,32 @@ const val SCHEMA_Q_NAME = "pydantic.schema.Schema"
 const val FIELD_Q_NAME = "pydantic.field.Field"
 const val BASE_SETTINGS_Q_NAME = "pydantic.env_settings.BaseSettings"
 
-fun getPyClassByPyCallExpression(pyCallExpression: PyCallExpression): PyClass? {
+internal fun getPyClassByPyCallExpression(pyCallExpression: PyCallExpression): PyClass? {
     return pyCallExpression.callee?.reference?.resolve() as? PyClass
 }
 
-fun getPyClassByPyKeywordArgument(pyKeywordArgument: PyKeywordArgument): PyClass? {
+internal fun getPyClassByPyKeywordArgument(pyKeywordArgument: PyKeywordArgument): PyClass? {
     val pyCallExpression = PsiTreeUtil.getParentOfType(pyKeywordArgument, PyCallExpression::class.java) ?: return null
     return getPyClassByPyCallExpression(pyCallExpression)
 }
 
-fun isPydanticModel(pyClass: PyClass, context: TypeEvalContext? = null): Boolean {
+internal fun isPydanticModel(pyClass: PyClass, context: TypeEvalContext? = null): Boolean {
     return isSubClassOfPydanticBaseModel(pyClass, context) || isPydanticDataclass(pyClass)
 }
 
-fun isPydanticBaseModel(pyClass: PyClass): Boolean {
+internal fun isPydanticBaseModel(pyClass: PyClass): Boolean {
     return pyClass.qualifiedName == BASE_MODEL_Q_NAME
 }
 
-fun isSubClassOfPydanticBaseModel(pyClass: PyClass, context: TypeEvalContext?): Boolean {
+internal fun isSubClassOfPydanticBaseModel(pyClass: PyClass, context: TypeEvalContext?): Boolean {
     return pyClass.isSubclass(BASE_MODEL_Q_NAME, context)
 }
 
-fun isBaseSetting(pyClass: PyClass, context: TypeEvalContext): Boolean {
+internal fun isBaseSetting(pyClass: PyClass, context: TypeEvalContext): Boolean {
     return pyClass.isSubclass(BASE_SETTINGS_Q_NAME, context)
 }
 
-fun hasDecorator(pyDecoratable: PyDecoratable, refName: String): Boolean {
+internal fun hasDecorator(pyDecoratable: PyDecoratable, refName: String): Boolean {
     pyDecoratable.decoratorList?.decorators?.mapNotNull { it.callee as? PyReferenceExpression }?.forEach {
         PyResolveUtil.resolveImportedElementQNameLocally(it).forEach { decoratorQualifiedName ->
             if (decoratorQualifiedName == QualifiedName.fromDottedString(refName)) return true
@@ -47,14 +48,21 @@ fun hasDecorator(pyDecoratable: PyDecoratable, refName: String): Boolean {
     return false
 }
 
-fun isPydanticDataclass(pyClass: PyClass): Boolean {
+internal fun isPydanticDataclass(pyClass: PyClass): Boolean {
     return hasDecorator(pyClass, DATA_CLASS_Q_NAME)
 }
 
-fun isPydanticField(pyClass: PyClass, context: TypeEvalContext): Boolean {
+internal fun isPydanticField(pyClass: PyClass, context: TypeEvalContext): Boolean {
     return pyClass.isSubclass(SCHEMA_Q_NAME, context) || pyClass.isSubclass(FIELD_Q_NAME, context)
 }
 
-fun validatorMethod(pyFunction: PyFunction): Boolean {
+internal fun validatorMethod(pyFunction: PyFunction): Boolean {
     return hasDecorator(pyFunction, VALIDATOR_Q_NAME)
+}
+
+internal fun getTypeHint(pyClass: PyClass, typeEvalContext: TypeEvalContext, pyTargetExpression: PyTargetExpression): String {
+    val className = pyClass.qualifiedName ?: pyClass.name
+    val defaultValue = pyTargetExpression.findAssignedValue()?.text?.let { "=$it" } ?: ""
+    val typeHint = PythonDocumentationProvider.getTypeHint(typeEvalContext.getType(pyTargetExpression), typeEvalContext)
+    return "${typeHint}$defaultValue $className"
 }
