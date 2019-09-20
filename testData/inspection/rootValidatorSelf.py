c7696030866f287989d862786be55635d0a8a05f
Koudai Aono <koxudaxi@gmail.com>
2019-09-21 02:30:05 +0900

    support root_validator (#72)
    * support root_validator

---
+++ testData/inspection/rootValidatorSelf.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel, root_validator
+
+def check(func):
+    def inner():
+        func()
+    return inner
+
+class A(BaseModel):
+    a: str
+    b: str
+    c: str
+    d: str
+    e: str
+
+    @root_validator('a')
+    def validate_a(<weak_warning descr="Usually first parameter of such methods is named 'cls'">self</weak_warning>):
+        pass
+
+    @root_validator('b')
+    def validate_b(<weak_warning descr="Usually first parameter of such methods is named 'cls'">fles</weak_warning>):
+        pass
+
+    @root_validator('c')
+    def validate_b(*args):
+        pass
+
+    @root_validator('d')
+    def validate_c(**kwargs):
+        pass
+
+    @root_validator('e')
+    def validate_e<error descr="Method must have a first parameter, usually called 'cls'">()</error>:
+        pass
+
+    def dummy(self):
+        pass
+
+    @check
+    def task(self):
+        pass
