ad6fda98580000e8b077d38834e38f6ac067bd99
Koudai Aono <koxudaxi@gmail.com>
2019-11-28 00:57:18 +0900

    fix dataclass inspection

--- testData/inspection/acceptsOnlyKeywordArguments.py
+++ testData/inspection/acceptsOnlyKeywordArguments.py
@@ -1,8 +1,15 @@
 from pydantic import BaseModel
-
+from pydantic.dataclasses import dataclass
 
 class A(BaseModel):
     a: str
 
 
 A(<warning descr="class 'A' accepts only keyword arguments">'a'</warning>)
+
+@dataclass
+class B():
+    a: str
+
+
+B('a')
