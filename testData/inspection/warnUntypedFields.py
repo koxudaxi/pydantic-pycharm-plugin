1695452faafad87b147a4dae1a6cfce276d1dbf4
Koudai Aono <koxudaxi@gmail.com>
2019-12-12 03:32:23 +0900

    add testcase

--- testData/inspection/warnUntypedFields.py
+++ testData/inspection/warnUntypedFields.py
@@ -4,3 +4,5 @@ from pydantic import BaseModel
 class A(BaseModel):
     <warning descr="Untyped fields disallowed">a = '123'</warning>
 
+class B(BaseModel):
+    b = '123'
--- testData/inspection/warnUntypedFieldsDisable.py
+++ testData/inspection/warnUntypedFieldsDisable.py
@@ -4,3 +4,6 @@ from pydantic import BaseModel
 class A(BaseModel):
     a = '123'
 
+
+class B(BaseModel):
+    b = '123'
