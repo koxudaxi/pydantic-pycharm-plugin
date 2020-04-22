d78089d5dd6917a5e861e0bb382e647aaa56d9e7
Koudai Aono <koxudaxi@gmail.com>
2020-04-23 02:22:55 +0900

    Add document link to plugin page (#108)
    * update plugin page
    
    * fix html link

--- src/com/koxudaxi/pydantic/PydanticConfigPanel.java
+++ src/com/koxudaxi/pydantic/PydanticConfigPanel.java
@@ -1,10 +1,19 @@
 package com.koxudaxi.pydantic;
 
 import com.intellij.ide.BrowserUtil;
+import com.intellij.ide.plugins.newui.EmptyCaret;
 import com.intellij.openapi.project.Project;
+import com.intellij.ui.ColorUtil;
+import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.UIUtil;
 
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
+import javax.swing.text.EditorKit;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.StyleSheet;
+
+import static com.intellij.util.ui.JBUI.CurrentTheme.NewClassDialog.panelBackground;
 
 public class PydanticConfigPanel {
 
@@ -14,13 +23,7 @@ public class PydanticConfigPanel {
         this.initTypedCheckBox.setSelected(pydanticConfigService.getInitTyped());
         this.warnUntypedFieldsCheckBox.setSelected(pydanticConfigService.getWarnUntypedFields());
 
-        this.textPane1.setText("<p style=\"font-family:Arial, Helvetica, sans-serif;font-size:130%;\">" +
-                "See <a href=\"https://koxudaxi.github.io/pydantic-pycharm-plugin/\">documentation</a> for more details.</p>");
-        this.textPane1.addHyperlinkListener(e -> {
-            if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-                BrowserUtil.browse(e.getURL());
-            }
-        });
+        setHyperlinkHtml(this.textPane1, "See <a href=\"https://koxudaxi.github.io/pydantic-pycharm-plugin/\">documentation</a> for more details.</p>");
     }
 
     private JPanel configPanel;
@@ -40,5 +43,33 @@ public class PydanticConfigPanel {
 
     public JPanel getConfigPanel() {
         return configPanel;
+    }
+
+    private void setHyperlinkHtml(JTextPane jTextPane, String html) {
+
+        jTextPane.setContentType("text/html");
+        jTextPane.setEditable(false);
+        jTextPane.setEditorKit(UIUtil.getHTMLEditorKit());
+
+        EditorKit kit = jTextPane.getEditorKit();
+        if (kit instanceof HTMLEditorKit) {
+            StyleSheet css = ((HTMLEditorKit) kit).getStyleSheet();
+
+            css.addRule("a, a:link {color:#" + ColorUtil.toHex(JBUI.CurrentTheme.Link.linkColor()) + ";}");
+            css.addRule("a:visited {color:#" + ColorUtil.toHex(JBUI.CurrentTheme.Link.linkVisitedColor()) + ";}");
+            css.addRule("a:hover {color:#" + ColorUtil.toHex(JBUI.CurrentTheme.Link.linkHoverColor()) + ";}");
+            css.addRule("a:active {color:#" + ColorUtil.toHex(JBUI.CurrentTheme.Link.linkPressedColor()) + ";}");
+            css.addRule("body {background-color:#" + ColorUtil.toHex(JBUI.CurrentTheme.DefaultTabs.background()) + ";}");
+        }
+
+        jTextPane.setText(html);
+        jTextPane.setCaret(EmptyCaret.INSTANCE);
+        jTextPane.setBackground(UIUtil.TRANSPARENT_COLOR);
+        jTextPane.setOpaque(false);
+        jTextPane.addHyperlinkListener(e -> {
+            if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                BrowserUtil.browse(e.getURL());
+            }
+        });
     }
 }
