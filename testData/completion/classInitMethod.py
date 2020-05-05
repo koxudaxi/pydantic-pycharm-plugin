59887b05d4d0148f33252c8c6a470bfdc5ca0624
Koudai Aono <koxudaxi@gmail.com>
2020-05-06 02:38:58 +0900

    Fix invalid self parameter when inherits from non-pydantic model

--- testData/completion/classInitMethod.py
+++ testData/completion/classInitMethod.py
@@ -5,7 +5,11 @@ from pydantic import BaseModel
 class A(BaseModel):
     abc: str
 
-class B(A):
+class B:
+    opq: str
+    xyz: str = '123'
+
+class C(A, B):
     efg: str
     def __init__(self):
         return self.<caret>
