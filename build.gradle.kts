019c2c1af26fb5f30172cb60401eec2b01009df8
Koudai Aono <koxudaxi@gmail.com>
2022-11-23 12:15:01 +0900

    Fix kotlin jvm target option

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,6 +113,11 @@ tasks {
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
         channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
     }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
 }
 
 dependencies {
