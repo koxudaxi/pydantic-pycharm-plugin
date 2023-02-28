7c991ea73c14e39deef98ae5c37f8538535c5672
Koudai Aono <koxudaxi@gmail.com>
2023-03-01 02:51:31 +0900

    Fix insert args

--- src/com/koxudaxi/pydantic/PydanticAnnotator.kt
+++ src/com/koxudaxi/pydantic/PydanticAnnotator.kt
@@ -6,12 +6,12 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.util.containers.nullize
 import com.jetbrains.python.psi.PyCallExpression
 import com.jetbrains.python.psi.PyStarArgument
+import com.jetbrains.python.psi.types.PyCallableType
 import com.jetbrains.python.psi.types.TypeEvalContext
 import com.jetbrains.python.validation.PyAnnotator
 
 
 class PydanticAnnotator : PyAnnotator() {
-    private val pydanticTypeProvider = PydanticTypeProvider()
     override fun visitPyCallExpression(node: PyCallExpression) {
         super.visitPyCallExpression(node)
         annotatePydanticModelCallableExpression(node)
@@ -19,14 +19,14 @@ class PydanticAnnotator : PyAnnotator() {
 
     private fun annotatePydanticModelCallableExpression(pyCallExpression: PyCallExpression) {
         val context = TypeEvalContext.codeAnalysis(pyCallExpression.project, pyCallExpression.containingFile)
+        val pyClassType = pyCallExpression.callee?.getType(context) as? PyCallableType ?: return
+        val pyClass = pyClassType.getReturnType(context)?.pyClassTypes?.firstOrNull()?.pyClass ?: return
+        if (!isPydanticModel(pyClass, true, context)) return
+        if (getPydanticModelInit(pyClass, context) != null) return
         if (!pyCallExpression.isDefinitionCallExpression(context)) return
-        val pyClassType = pyCallExpression.callee?.getType(context)?.pyClassTypes?.firstOrNull()
-        val pyClass = pyClassType?.pyClass ?: return
-        if (getPydanticModelInit(pyClassType.pyClass, context) != null) return
-        val pydanticType =  pydanticTypeProvider.getPydanticTypeForClass(pyClass, context, true, pyCallExpression) ?: return
 
         val unFilledArguments =
-            getPydanticUnFilledArguments(pydanticType, pyCallExpression, context).nullize()
+            getPydanticUnFilledArguments(pyClassType, pyCallExpression, context).nullize()
                 ?: return
         holder.newSilentAnnotation(HighlightSeverity.INFORMATION).withFix(PydanticInsertArgumentsQuickFix(false))
             .create()
