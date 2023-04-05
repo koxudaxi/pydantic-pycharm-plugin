c8b0aea6c76983476756c08bca8db99a0e61510a
Koudai Aono <koxudaxi@gmail.com>
2023-04-06 01:24:52 +0900

    Add links

--- src/com/koxudaxi/pydantic/PydanticDocumentationLinkProvider.kt
+++ src/com/koxudaxi/pydantic/PydanticDocumentationLinkProvider.kt
@@ -9,7 +9,14 @@ import java.util.function.Function
 
 class PydanticDocumentationLinkProvider : PythonDocumentationLinkProvider {
     private val urls = mapOf(
-        BASE_MODEL_Q_NAME to "https://docs.pydantic.dev/usage/models/#basic-model-usage"
+        BASE_MODEL_Q_NAME to "https://docs.pydantic.dev/usage/models/#basic-model-usage",
+        VALIDATOR_Q_NAME to "https://docs.pydantic.dev/usage/validators/",
+        VALIDATOR_SHORT_Q_NAME to "https://docs.pydantic.dev/usage/validators/",
+        ROOT_VALIDATOR_Q_NAME to "https://docs.pydantic.dev/usage/validators/#root-validators",
+        ROOT_VALIDATOR_SHORT_Q_NAME to "https://docs.pydantic.dev/usage/validators/#root-validators",
+        DATA_CLASS_Q_NAME to "https://docs.pydantic.dev/usage/dataclasses/",
+        BASE_SETTINGS_Q_NAME to "https://docs.pydantic.dev/usage/settings/",
+        BASE_CONFIG_Q_NAME to "https://docs.pydantic.dev/usage/model_config/"
     )
 
     override fun getExternalDocumentationUrl(element: PsiElement, originalElement: PsiElement): String? {
