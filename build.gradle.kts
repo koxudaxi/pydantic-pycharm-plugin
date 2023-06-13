c58d8fe9f9b9ab49311926a5703680a74e597477
Koudai Aono <koxudaxi@gmail.com>
2023-06-13 18:58:35 +0900

    # Support Kotlin 1.8.20 -> https://plugins.jetbrains.com/docs/intellij/using-kotlin.html#incremental-compilation kotlin.incremental.useClasspathSnapshot=false

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ fun environment(key: String) = providers.environmentVariable(key)
 
 plugins {
     // Kotlin support
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.8.20"
     // Gradle IntelliJ Plugin
     id("org.jetbrains.intellij") version "1.14.1"
     // Gradle Changelog Plugin
