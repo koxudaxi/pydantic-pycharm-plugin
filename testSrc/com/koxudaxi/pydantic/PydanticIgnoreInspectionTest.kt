2cd49932f4dde0c3137748f59915883d358fc612
Koudai Aono <koxudaxi@gmail.com>
2023-07-20 10:11:36 +0900

    add unittest

--- testSrc/com/koxudaxi/pydantic/PydanticIgnoreInspectionTest.kt
+++ testSrc/com/koxudaxi/pydantic/PydanticIgnoreInspectionTest.kt
@@ -1,5 +1,7 @@
 package com.koxudaxi.pydantic
 
+import com.intellij.psi.PsiReference
+import com.jetbrains.python.psi.PyElement
 import com.jetbrains.python.psi.PyFunction
 import com.jetbrains.python.psi.types.TypeEvalContext
 
@@ -7,7 +9,7 @@ import com.jetbrains.python.psi.types.TypeEvalContext
 open class PydanticIgnoreInspectionTest : PydanticTestCase() {
 
 
-    private fun doTest(expected: Boolean) {
+    private fun doIgnoreMethodParametersTest(expected: Boolean) {
         configureByFile()
         val pyFunction = myFixture!!.elementAtCaret as PyFunction
         val context = TypeEvalContext.codeInsightFallback(myFixture!!.project)
@@ -15,32 +17,48 @@ open class PydanticIgnoreInspectionTest : PydanticTestCase() {
         assertEquals(expected, actual)
     }
 
+    private fun doIgnoreUnresolvedReference(expected: Boolean) {
+        configureByFile()
+        val pyElement = myFixture!!.elementAtCaret as PyElement
+        val psiReference = pyElement.reference as PsiReference
+        val context = TypeEvalContext.codeInsightFallback(myFixture!!.project)
+        val actual = PydanticIgnoreInspection().ignoreUnresolvedReference(pyElement, psiReference, context)
+        assertEquals(expected, actual)
+    }
+
     fun testValidator() {
-        doTest(true)
+        doIgnoreMethodParametersTest(true)
     }
 
     fun testValidatorFullPath() {
-        doTest(true)
+        doIgnoreMethodParametersTest(true)
     }
 
     fun testValidatorDataclass() {
-        doTest(true)
+        doIgnoreMethodParametersTest(true)
     }
 
 
     fun testBaseModel() {
-        doTest(false)
+        doIgnoreMethodParametersTest(false)
     }
 
     fun testPythonFunction() {
-        doTest(false)
+        doIgnoreMethodParametersTest(false)
     }
 
     fun testPythonMethod() {
-        doTest(false)
+        doIgnoreMethodParametersTest(false)
     }
 
     fun testPythonDecorator() {
-        doTest(false)
+        doIgnoreMethodParametersTest(false)
+    }
+    fun testValidatorField() {
+        doIgnoreUnresolvedReference(true)
+    }
+
+    fun testDecoratorField() {
+        doIgnoreUnresolvedReference(false)
     }
 }
