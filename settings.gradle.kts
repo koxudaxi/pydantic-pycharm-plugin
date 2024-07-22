89261d0d41228689dc7ff308482f706e3f5e97db
Koudai Aono <koxudaxi@gmail.com>
2024-07-22 18:54:22 +0000

    Revert "Support 242 EAP"
    This reverts commit 0780a0cfb3b49aef134643b563d2e925a29b50d3.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,3 @@
-pluginManagement {
-    repositories {
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
-        gradlePluginPortal()
-    }
-    plugins {
-        kotlin("jvm") version "2.0.0"
-    }
-}
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
