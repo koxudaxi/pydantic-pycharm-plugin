3ddd904af081cd078eef50403bb48c1818242536
Koudai Aono <koxudaxi@gmail.com>
2024-11-10 03:14:05 +0900

    update unittest

--- testData/typeinspection/fieldUnionInvalid.py
+++ testData/typeinspection/fieldUnionInvalid.py
@@ -1,12 +1,10 @@
-
 from typing import Union
 
 from pydantic import BaseModel
 
 
 class A(BaseModel):
-    a: Union[float, int]
+    a: Union[str, bytes]
 
 
-A(<warning descr="Expected type 'Union[float, int]', got 'bytes' instead">a=bytes(123)</warning>)
-A(<warning descr="Expected type 'Union[float, int]', got 'str' instead">a=str('123')</warning>)
+A(<warning descr="Expected type 'Union[str, bytes]', got 'int' instead">a=int(123)</warning>)
