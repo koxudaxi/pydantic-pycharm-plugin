4cdf0b24dcd20276fa13ff46cac2ac4b0eed87f0
Koudai Aono <koxudaxi@gmail.com>
2025-12-11 12:58:42 +0900

    Fix: Suppress dataclass inspection for Pydantic models

--- src/com/koxudaxi/pydantic/PydanticPythonPackageManagementListener.kt
+++ src/com/koxudaxi/pydantic/PydanticPythonPackageManagementListener.kt
@@ -2,24 +2,38 @@ package com.koxudaxi.pydantic
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.platform.ide.progress.withBackgroundProgress
 import com.jetbrains.python.packaging.common.PythonPackageManagementListener
 import com.jetbrains.python.sdk.PythonSdkUtil
 import com.jetbrains.python.sdk.PythonSdkUtil.isDisposed
 import com.jetbrains.python.statistics.sdks
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class PydanticPythonPackageManagementListener : PythonPackageManagementListener {
+    private val scope = CoroutineScope(Dispatchers.Default)
+
     private fun updateVersion(sdk: Sdk) {
-        ProjectManager.getInstance().openProjects
-            .filter { it.sdks.contains(sdk) }
-            .forEach {
-                PydanticCacheService.clear(it)
-                val version = getPydanticVersion(it, sdk)
+        val projects = ProjectManager.getInstance().openProjects.filter { it.sdks.contains(sdk) }
+        for (project in projects) {
+            updateVersionForProject(project, sdk)
+        }
+    }
+
+    private fun updateVersionForProject(project: Project, sdk: Sdk) {
+        PydanticCacheService.clear(project)
+        scope.launch {
+            withBackgroundProgress(project, "Updating Pydantic version cache") {
+                val version = getPydanticVersion(project, sdk)
                 if (version is String) {
-                    PydanticCacheService.getOrPutVersionFromVersionCache(it, version)
+                    PydanticCacheService.getOrPutVersionFromVersionCache(project, version)
                 }
             }
+        }
     }
 
     override fun packagesChanged(sdk: Sdk) {
