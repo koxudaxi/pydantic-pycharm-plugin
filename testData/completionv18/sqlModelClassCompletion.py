9987edf6a9e8c5097751107eb6f0372aed1b4d9d
Koudai Aono <koxudaxi@gmail.com>
2025-12-14 01:13:04 +0900

    Fix: Enhance SQLModel class completion and implement SDK cache invalidation

---
+++ testData/completionv18/sqlModelClassCompletion.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
+
+class Hero(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str = Field(default="dummy", primary_key=True)
+    age: Optional[int] = None
+
+
+Hero.<caret>
+
