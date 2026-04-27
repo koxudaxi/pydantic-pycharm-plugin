9a460b2709c77a800f7bacc36624471c271758d7
Fayyazul Hassan <fayyazul@hotmail.com>
2026-04-26 22:51:44 -0500

    update sqlmodel type inspection test to test parameters for instantiation

--- testData/sqlmodel/typeInspection.py
+++ testData/sqlmodel/typeInspection.py
@@ -16,6 +16,9 @@ class TableModel(SQLModel, table=True):
 non_table_instance = NonTableModel(id=1, name="non_table_instance")
 table_instance = TableModel(id=1, name="table_instance")
 
+omitting_unrequired_parameter = TableModel(name="omitting_unrequired_parameter")
+missing_required_parameter = TableModel(id=1<warning descr="null">)</warning>
+incorrect_types_for_parameters = TableModel(<warning descr="Expected type 'Optional[int]', got 'str' instead">id="id"</warning>, <warning descr="Expected type 'str', got 'int' instead">name=1</warning>)
 
 def f(x: Optional[int]) -> None:
     pass
