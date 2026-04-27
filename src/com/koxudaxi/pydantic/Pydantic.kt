6c43aa0edb92fa5ca2959dd713f2a8d5b8d08986
Fayyazul Hassan <fayyazul@hotmail.com>
2026-04-27 06:17:48 -0500

    flip order of conditions for quicker short-circuiting

--- src/com/koxudaxi/pydantic/Pydantic.kt
+++ src/com/koxudaxi/pydantic/Pydantic.kt
@@ -280,10 +280,9 @@ internal fun isSubClassOfCustomBaseModel(pyClass: PyClass, context: TypeEvalCont
 
 internal fun isTableSqlModel(pyClass: PyClass, context: TypeEvalContext): Boolean {
     // class Hero(SQLModel, table=True): ...
-    return pyClass.isSubclass(SQL_MODEL_Q_NAME, context)
-            && pyClass.superClassExpressions.any {
+    return pyClass.superClassExpressions.any {
         it is PyKeywordArgument && it.keyword == "table" && (it.value as? PyBoolLiteralExpression)?.value == true
-    }
+    } && pyClass.isSubclass(SQL_MODEL_Q_NAME, context)
 }
 
 internal val PyClass.isBaseSettings: Boolean get() = qualifiedName in BASE_SETTINGS_Q_NAMES
